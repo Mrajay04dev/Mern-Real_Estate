5be5a1d8be9a1db65495fc407cda0cf6d447af4e
Mrajay04dev <ajay177081@gmail.com>
2023-12-24 11:56:09 +0530

    Sign in api route

--- api/controller/authcontroller.js
+++ api/controller/authcontroller.js
@@ -24,13 +24,13 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validUser) return next(errorHandler(401, "Wrong Credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-
+    const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .json(validUser);
+      .json(rest);
   } catch (error) {
     next(error);
   }
